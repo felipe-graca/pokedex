418de302246f1bee833bebd32c78adb63e3d51d4
Felipe <felipegraca.1999@gmail.com>
2023-02-23 09:25:05 -0300

    Feature: add google fonts package; Create splash Screen animation; add logo image to projec;

--- lib/features/pages/splash_page/splash_page.dart
+++ lib/features/pages/splash_page/splash_page.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -8,9 +7,35 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  var topMovement = false;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 800));
+    Future.delayed(const Duration(milliseconds: 800))
+        .then((value) => {topMovement = true});
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            top: topMovement ? 0 : width / 2,
+            duration: controller.duration!,
+            width: 400,
+            child: Image.asset('assets/images/logo.png'),
+          ),
+        ],
+      ),
+    );
   }
 }
